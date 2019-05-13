ae16e7cc1d7b63c3c2ea1b0606273318194ff574
Anna Dybas <a.m.dybas@gmail.com>
2019-05-13 17:54:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\A[aeiouAEIOU]/) ? true : false
+  word.scan(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
